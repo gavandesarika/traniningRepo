8d366efecdf119ea21d4c3b816150dcc93f708e9
gavandesarika <sarika.gavande@neosoftmail.com>
2020-12-18 21:24:17 -0800

    Jsx demo

--- jsxdemo/src/App.js
+++ jsxdemo/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello'
 
 function App() {
   return (
     <div className="App">
-    
-    <div>welcome Sarika </div> 
+   <Hello />
     </div>
   );
 }
